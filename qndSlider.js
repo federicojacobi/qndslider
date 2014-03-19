fa262bfa7431b0a1b33fe666b3c83f4755306b4a
federicojacobi <federico_jacobi@yahoo.com>
2014-03-19 14:57:40 -0400

    Update qndSlider.js

--- qndSlider.js
+++ qndSlider.js
@@ -3,39 +3,61 @@ jQuery("document").ready( function() {
 	function Slider( id ) {
 		this.delay = 500;
 		this.slides = jQuery( id ).children(".slide");
+		this.slides.hide();
+		this.slides.first().show();
 		this.currentSlide = 1;
 		this.timer = null;
 		
 		this.next = function() {
 			if ( this.slides.length < 2 ) return false;
-			jQuery( this.getSlide( this.currentSlide ) ).fadeOut( this.delay ).removeClass("active");
+			var oldSlide = this.getSlide( this.currentSlide );
+			
 			if ( this.currentSlide + 1 > this.slides.length ) {
 				this.currentSlide = 1;
 			} else {
 				this.currentSlide++;
 			}
-			jQuery( this.getSlide( this.currentSlide ) ).fadeIn( this.delay ).addClass("active");
+			var newSlide = this.getSlide( this.currentSlide );
+			
+			jQuery( oldSlide )
+				.fadeOut( this.delay, function() {
+					jQuery( newSlide ).fadeIn( this.delay ).addClass("active");
+				})
+				.removeClass("active");
 		};
 		
 		this.prev = function() {
 			if ( this.slides.length < 2 ) return false;
-			jQuery( this.getSlide( this.currentSlide ) ).fadeOut( this.delay ).removeClass("active");
+			var oldSlide = this.getSlide( this.currentSlide );
+			
 			if ( this.currentSlide - 1 < 1 ) {
 				this.currentSlide = this.slides.length;
 			} else {
 				this.currentSlide--;
 			}
-			jQuery( this.getSlide( this.currentSlide ) ).fadeIn( this.delay ).addClass("active");;
+			var newSlide = this.getSlide( this.currentSlide );
+			
+			jQuery( oldSlide )
+				.fadeOut( this.delay, function() {
+					jQuery( newSlide ).fadeIn( this.delay ).addClass("active");
+				})
+				.removeClass("active");
 		};
 		
 		this.goto = function( n, stop ) {
 			if ( n > this.slides.length || n < 1 )	return;
 			if ( stop !== false )
 				this.stopTimer();
-			
-			jQuery( this.getSlide( this.currentSlide ) ).fadeOut( this.delay ).removeClass("active");
+				
+			var oldSlide = this.getSlide( this.currentSlide );
 			this.currentSlide = n;
-			jQuery( this.getSlide( this.currentSlide ) ).fadeIn( this.delay ).addClass("active");
+			var newSlide = this.getSlide( this.currentSlide );
+			
+			jQuery( oldSlide )
+				.fadeOut( this.delay, function() {
+					jQuery( newSlide ).fadeIn( this.delay ).addClass("active");
+				})
+				.removeClass("active");
 		};
 		
 		this.startTimer = function( t, callback ) {
@@ -54,34 +76,34 @@ jQuery("document").ready( function() {
 			return this.slides[n-1];
 		};
 	}
-
-	var awardSlider = new Slider( "#awards" );
-	jQuery( "#awards .controls .first" ).click( function() { 
+	
+	var mySlider = new Slider( "#theSlider" );
+	jQuery( "#theSlider .controls .first" ).click( function() { 
 		jQuery(this).addClass( "active").siblings().removeClass( "active" );
 		awardSlider.goto(1);
 	});
-	jQuery( "#awards .controls .second" ).click( function() {
+	jQuery( "#theSlider .controls .second" ).click( function() {
 		jQuery(this).addClass( "active").siblings().removeClass( "active" );
 		awardSlider.goto(2);
 	});
-	jQuery( "#awards .controls .third" ).click( function() {
+	jQuery( "#theSlider .controls .third" ).click( function() {
 		jQuery(this).addClass( "active").siblings().removeClass( "active" );
 		awardSlider.goto(3);
 	});
-	jQuery( "#awards .controls .forth" ).click( function() {
+	jQuery( "#theSlider .controls .forth" ).click( function() {
 		jQuery(this).addClass( "active").siblings().removeClass( "active" );
 		awardSlider.goto(4);
 	});
-	jQuery( "#awards .controls .fifth" ).click( function() {
+	jQuery( "#theSlider .controls .fifth" ).click( function() {
 		jQuery(this).addClass( "active").siblings().removeClass( "active" );
 		awardSlider.goto(5);
 	});
 	awardSlider.startTimer( 3000, function() {
-			jQuery( "#awards .controls span" ).removeClass( "active" );
-			jQuery( "#awards .controls span:nth-child(" + awardSlider["currentSlide"] + ")" ).addClass( "active");
+			jQuery( "#theSlider .controls span" ).removeClass( "active" );
+			jQuery( "#theSlider .controls span:nth-child(" + awardSlider["currentSlide"] + ")" ).addClass( "active");
 	});
 
-	var slider = new Slider( "#slider" );			
+	var slider = new Slider( "#theSlider2" );			
 	jQuery( "#slider .larrow" ).click( function() {
 		slider.prev();
 	});
