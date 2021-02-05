f8ded2fed606de548a96928f7eb4ec2dd721988f
Federico Jacobi <fjacobi@bhmdigital.com>
2021-02-04 19:53:26 -0500

    Remove jquery dependency. Refactor with modern JS

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 Quick and Dirty Slider
 ======================
-A JS Developer friendly slider. Of course you can code it yourself, but i already did it for you.
-
-This is NOT a jQuery plugin, but it uses jQuery (who doesn&#39;t nowadays), so you must add the code inside of jQuery(document).ready()
+A JS Developer friendly slider. Of course you can code it yourself, but i already did it for you. It is not a jQuery plugin, and does not need it.
 
 The whole purpose is to cut the bloat and do a quick slider with fade in, fade out.
 
 NEXT / PREV arrows are optional and are not built by the slider, giving you control over how they work, but available via .next() and .prev() methods.
 
-In the same fashion if you need to show a specific slide just do .goto(4) to show the 4th slide.
+In the same fashion if you need to show a specific slide just do .goto(3) to show the 4th slide (because the world zero-based).
 
-When the slider loads, all the slides are hidden ( jQuery(slides).hide() ) and then first one is shown.
+When the slider loads, all the slides but the first one are hidden ( display:none; opacity: 0 ). The script will add the style, but you should do this in CSS to prevent Cumulative Layout Shift.
 
 HOW TO
 ======
@@ -36,9 +34,9 @@ OR you can do (but DIVS give you more flexibility, use whatever makes you happy)
 
 Then in JS
 ```
-// you can use a jQuery selector as a parameter as in "body .content > div" or whatever
-var GreatSlider = new Slider( "yourName" );
-var anotherSlider = new Slider( "mySlider" );
+// Use the element id for the parameter. Do not use a selector.
+var GreatSlider = new Slider( 'yourName' );
+var anotherSlider = new Slider( 'mySlider' );
 ```
 Notice the lack of CSS ... that&#39;s on purpose so you can do your own, and understand it !!!
 
@@ -48,57 +46,44 @@ Controls for next, previous and go to are separate from the main logic and you c
 ```
 <a href"#" onclick="GreatSlider.next()" >NEXT</a> -- <a href="#" onclick="GreatSlider.prev()" >PREV</a>
 
-<a href="#" onclick="GreatSlider.goto(3)" >Go to slide 3</a>
+<a href="#" onclick="GreatSlider.goto(2)" >Go to slide 3</a>
 ```
-You could do some jQuery magic an attach the click event to something else (assume the control button is inside of the main div wrapper) :
-
-	jQuery( "#yourName .prev-button" ).click( function() {
+You could attach the click event to another element (assume the control button is inside of the main div wrapper) :
+```
+	document.querySelector( "#yourName .prev-button" ).addEventListener( 'click', function() {
 		GreatSlider.prev();
-	});
+	} );
+```
 
-If your slides are created dynamically and you don't know how many slides there are, but still want to do GOTO controls ... no problem ... a bit of jQuery will do it:
+If your slides are created dynamically and you don't know how many slides there are, but still want to do GOTO controls ... no problem, you could do something like this:
 
-	jQuery( "#featured_slider .controls li" ).click( function() { 
-		jQuery(this).addClass( "active" ).siblings().removeClass( "active" );
-		featuredSlider.goto( jQuery( this ).index() + 1 );
-	});
-	
-In this case, your slider is #featured_slider with the controls in a div .controls and inside of a ul/li. The will remove the &quot;active&quot; class from all the LIs, add it to the clicked control, and go to that slide. That way your active control has the &quot;active&quot; class. The main slider always has the &quot;active&quot; class on the visible slide.
+```
+	unknownSlider = new Slider( 'the_unknown' );
+	unknownSlider.goto( unknownSlider.slides.length - 1 ); // go to the last slide.
+```
 
 TIMERS
 ======
 Of course your slider needs a timer. But it is not active by default. You need to specify when to fire it:
-
+```
 GreatSlider.startTimer()   Defaults to 500ms delay
 GreatSlider.startTimer( 3000 );    3000ms delay
 GrearSlider.startTimer( 3000, function() { });    3000ms delay and AFTER the the slide is changes function is called. Notice the call happens after the doAfter() callback (see below).
-
+```
 The last line is great if you need to do something in particular after the timer clicks, but not when the other controls are clicked:
-
+```
 	GreatSlider.startTimer( 5000, function() {
 			console.log('The timer has clicked');
 	});
-	
-If you want to pause on HOVER, there&#39;s no pause, BUT there&#39;s .stopTimer() so you can do:
-
-	jQuery( "#featured_slider" ).hover( 
-		function() { 
-			featuredSlider.stopTimer();
-		}, 
-		function() {
-			featuredSlider.startTimer( 5000 );
-		}
-	);
+```
+If you want to pause on HOVER, there&#39;s no pause, BUT there&#39;s .stopTimer() and you can combine with .startTimer().
 
 doBefore AND doAfter
 ====================
 These are callbacks that happen before and after the slide is changed. Useful when you need to update you external controls:
-
-	GreatSlider.doAfter = function( currentSlideElement ) {
-		jQuery( "#featured_slider ul.controls li" ).removeClass( "active" );
-		jQuery( "#featured_slider ul.controls li:nth-child(" + featuredSlider["currentSlide"] + ")" ).addClass( "active");
+```
+	GreatSlider.on( 'afterSlideChange', function( currentSlideElement ) {
+		// currentSlideElement is a DOM node corresponding to a slide element.
 	};
-	
-Returning false in doBefore will stop switching the slide (and in turn triggering doAfter).
-
+```
 doBefore and doAfter are called whenever you do .next(), .prev() and .goto(). 
--- qndSlider.js
+++ qndSlider.js
@@ -1,114 +1,171 @@
-jQuery(document).ready( function() {
-
-	function Slider( id ) {
-		
-		this.init = function() {
-			this.slides.hide();
-			this.slides.first().show();
-			this.ready = true;
-		};
-		
-		this.next = function() {
-			if ( this.slides.length < 2 || ! this.ready ) return false;
-			var oldSlide = this.getSlide( this.currentSlide );
-			
-			if ( this.currentSlide + 1 > this.slides.length ) {
-				this.currentSlide = 1;
-			} else {
-				this.currentSlide++;
-			}
-			var newSlide = this.getSlide( this.currentSlide );
-			this.switch( oldSlide, newSlide );
-		};
-		
-		this.prev = function() {
-			if ( this.slides.length < 2 || ! this.ready ) return false;
-			var oldSlide = this.getSlide( this.currentSlide );
-			
-			if ( this.currentSlide - 1 < 1 ) {
-				this.currentSlide = this.slides.length;
-			} else {
-				this.currentSlide--;
-			}
-			var newSlide = this.getSlide( this.currentSlide );
-			this.switch( oldSlide, newSlide );
-		};
-		
-		this.goto = function( n, stop ) {
-			if ( this.ready ) {
-				if ( n > this.slides.length || n < 1 || ! this.ready )	return;
-				if ( stop !== false )
-					this.stopTimer();
-					
-				var oldSlide = this.getSlide( this.currentSlide );
-				this.currentSlide = n;
-				var newSlide = this.getSlide( this.currentSlide );
-				this.switch( oldSlide, newSlide );
-			}
-		};
-		
-		this.switch = function( from, to ) {
-			var obj = this;
-			var keepGoing = true;
-			
-			if ( typeof this.doBefore === 'function' )
-				keepGoing = this.doBefore( from );
-			
-			if ( keepGoing !== false ) {
-					this.ready = false;
-					jQuery( from )
-						.fadeOut( this.delay, function() {
-							jQuery( to ).fadeIn( obj.delay, function() { obj.ready = true;} ).addClass("active");
-							if ( typeof obj.doAfter === 'function' )
-								obj.doAfter( to );
-						})
-						.removeClass("active");
-			}
-		};
-		
-		this.startTimer = function( t, callback ) {
-			var obj = this;
-			if ( this.timer != null ) {
-				this.stopTimer();
-			}
-			if ( t == null )
-				t = this.timerWait;
-			else {
-				if ( t < 125 ) t = this.timerWait;
-			}
-			this.timerWait = t;
-			
-			this.timer = setInterval( 
-				function() {
-					obj.next();
-					if ( typeof callback === 'function' )
-						callback(); 
-				} , t);
-			return this.timer;
-		};
-		
-		this.stopTimer = function() {
-			clearInterval( this.timer );
-		};
-		
-		this.getSlide = function( n ) {
-			return this.slides[n-1];
-		};
-		
-		this.slides = jQuery( id ).children(".slide");
-		
-		this.delay = 500;
-		this.currentSlide = 1;
+class Slider {
+	constructor( id ) {
+		this.container = document.getElementById( id );
+		this.slides = this.container.querySelectorAll( '.slide' );
+	
+		this.delay = 250;
+		this.currentSlide = 0;
 		
 		this.timer = null;
-		this.timerWait = 1000;
 		
-		this.doBefore = null;
-		this.doAfter = null;
-		this.ready = false;
-		
-		this.init();
+		this.slides.forEach( ( slide, index ) => {
+			if ( index > 0 ) {
+				slide.style.display = 'none';
+				slide.style.opacity = 0;
+			}
+		} );
+
+		this.ready = true;
+
+		this._events = {};
+	}
+
+	on( name, listener ) {
+		if ( ! this._events[ name ] ) {
+			this._events[ name ] = [];
+		}
+
+		this._events[ name ].push( listener );
+	}
+
+	off( name, listener ) {
+		this._events[ name ] = this._events[name].filter( l => l !== listener );
+	}
+
+	trigger( name, data ) {
+		if ( this._events[ name ] ) {
+			this._events[ name ].forEach( callback => { callback( data ) } );
+		}
 	}
 	
-	// All your mods/code goes here ... including the creation of the slider as in var mySlider = new Slider('#mySlider');
-});
+	next() {
+		if ( ! this.ready || 0 === this.slides.length ) {
+			return false;
+		}
+
+		var oldSlide = this.slides[ this.currentSlide ];
+		
+		if ( this.currentSlide === this.slides.length - 1 ) {
+			this.currentSlide = 0;
+		} else {
+			this.currentSlide++;
+		}
+
+		this.switch( oldSlide, this.slides[ this.currentSlide ] );
+	}
+	
+	prev() {
+		if ( ! this.ready || 0 === this.slides.length ) {
+			return false;
+		}
+
+		var oldSlide = this.slides[ this.currentSlide ];
+		
+		if ( 0 === this.currentSlide ) {
+			this.currentSlide = this.slides.length - 1;
+		} else {
+			this.currentSlide--;
+		}
+
+		this.switch( oldSlide, this.slides[ this.currentSlide ] );
+	}
+	
+	goto( n, stop ) {
+		if ( this.ready ) {
+			if ( n >= this.slides.length || n < 0 || ! this.ready ) {
+				return;
+			}
+
+			if ( stop !== false ) {
+				this.stopTimer();
+			}
+				
+			var oldSlide = this.slides[ this.currentSlide ];
+			this.currentSlide = n;
+			var newSlide = this.slides[ this.currentSlide ];
+			this.switch( oldSlide, newSlide );
+		}
+	}
+	
+	animate( options ) {
+		if ( ! options.el ) {
+			return;
+		}
+
+		let lerp = ( start, end, amt ) => ( 1 - amt ) * start + amt * end;
+		let clamp = ( num, a, b ) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
+
+		let initialOpacity = window.getComputedStyle( options.el ).opacity;
+		let targetOpacity = options.targetOpacity;
+
+		let length = options.time || 250;
+
+		let startTime = performance.now();
+		let animation = ( time ) => {
+			let elapsed = time - startTime;
+			let pct = clamp( elapsed / length, 0, 1 );
+			
+			options.el.style.opacity = lerp( initialOpacity, targetOpacity, pct );
+
+			if ( elapsed < length ) {
+				window.requestAnimationFrame( animation );
+			} else {
+				if ( options.callback ) {
+					options.callback();
+				}
+			}
+		}
+		window.requestAnimationFrame( animation );
+	}
+
+	switch( from, to ) {
+		this.trigger( 'beforeSlideChange', from );
+		
+		this.ready = false;
+		this.animate( {
+			el: from,
+			targetOpacity: 0,	// fadeOut
+			time: this.delay,
+			callback: () => {
+				from.style.display = 'none';
+				from.classList.remove( 'active' );
+				to.style.display = 'block';
+				to.style.opacity = 0;
+
+				this.animate( {
+					el: to,
+					targetOpacity: 1,	// fadeIn
+					callback: () => {
+						to.classList.add( 'active' );
+						this.ready = true;
+						this.trigger( 'afterSlideChange', to );
+					}
+				} );
+			}
+		} );
+	}
+	
+	startTimer( t = 3000, callback = null ) {
+		if ( this.timer != null ) {
+			this.stopTimer();
+		}
+
+		if ( t < 125 ) {
+			t = 125;
+		}
+		
+		this.timer = setInterval(
+			() => {
+				this.next();
+				if ( null !== callback ) {
+					callback();
+				} 
+			}, t );
+
+		return this.timer;
+	}
+	
+	stopTimer() {
+		clearInterval( this.timer );
+	}
+}
